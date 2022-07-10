7a6066b5f96c2c6d97d3b898b29d340a718f3976
unknown <melkonyan.anatoliy@gmail.com>
2022-07-10 20:07:02 +0300

    списки

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ a = int(input('-->'))
 b = int(input('-->'))
 c = int(input('-->'))
 if a > b and a > c:
-    print(a)
-elif b < a and b > c:
-    print(b)
+  print(a)
+elif b > a and b > c:
+  print(b)
 elif c > a and c > b:
-    print(c)
+  print(c)
+else:
+  if a == b and a > c:
+    print('Числа a, b максимальные')
+  elif a == c and a > b:
+    print('Числа a, c максимальные')
+  elif b == c and b > a:
+    print('Числа b, c максимальные')
+  else:
+    print('Числа равны')
