4fd8aec6c2605ebd711bdc38dae03f7648fae9ab
unknown <melkonyan.anatoliy@gmail.com>
2022-07-16 14:30:44 +0300

    аÐперсикÑ

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-talon = ''
-nom = 1
-while talon != '1':
-    talon = input('Введите 0 — получить талон, 1 — выключить аппарат-->')
-    if talon == '0':
-        print(nom)
-        nom += 1
-
+rek = ''
+aok = ''
+roz = ''
+while rek != 'off':
+    rek = input('1-рекомендации, 2-розыгрыш, off-выключение-->')
+    if rek == '1':
+        aok = input('Введите предпочтения-->')
+        if aok == 'Спорт':
+            print('FIFA 2018 Матч Россия/Испания')
+        elif aok == 'Музыка':
+            print('саундтреки DOOM')
+    elif rek == '2':
+         for i in range(3):
+             roz = input('За 3 попытки нужно отгадать название музыкальной группы («Queen»).')
+             if roz == 'Queen':
+                 print('Вы выиграли билет на концерт!')
+                 break
