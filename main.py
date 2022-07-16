52928ff412f065db25e7cafaa5302d54e8ae5825
unknown <melkonyan.anatoliy@gmail.com>
2022-07-16 13:33:26 +0300

    ÐÑпромокод

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
-a = int(input('-->'))
-b = int(input('-->'))
-c = int(input('-->'))
-if a > b and a > c:
-  print(a)
-elif b > a and b > c:
-  print(b)
-elif c > a and c > b:
-  print(c)
-else:
-  if a == b and a > c:
-    print('Числа a, b максимальные')
-  elif a == c and a > b:
-    print('Числа a, c максимальные')
-  elif b == c and b > a:
-    print('Числа b, c максимальные')
+for i in range(3):
+  prom = input('Введите промокод -->')
+  if prom != 'fresh':
+    print('Неверный промокод')
   else:
-    print('Числа равны')
+    print('Верный промокод')
+    print(i + 1)
+    break
+
