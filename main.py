f64ce0e2407005b5ec90c3de9e63649ec0001387
unknown <melkonyan.anatoliy@gmail.com>
2022-07-16 13:50:34 +0300

    ТTalon

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-for i in range(3):
-  prom = input('Введите промокод -->')
-  if prom != 'fresh':
-    print('Неверный промокод')
-  else:
-    print('Верный промокод')
-    print(i + 1)
-    break
+talon = ''
+nom = 1
+while talon != '1':
+    talon = input('Введите 0 — получить талон, 1 — выключить аппарат-->')
+    if talon == '0':
+        print(nom)
+        nom += 1
 
