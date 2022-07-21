f4a98de36c65088a5ad2484da0e17e767a257f73
unknown <melkonyan.anatoliy@gmail.com>
2022-07-21 23:22:23 +0300

    Ñсписки нахождение минимального числа

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
-rek = ''
-aok = ''
-roz = ''
-while rek != 'off':
-    rek = input('1-рекомендации, 2-розыгрыш, off-выключение-->')
-    if rek == '1':
-        aok = input('Введите предпочтения-->')
-        if aok == 'Спорт':
-            print('FIFA 2018 Матч Россия/Испания')
-        elif aok == 'Музыка':
-            print('саундтреки DOOM')
-    elif rek == '2':
-         for i in range(3):
-             roz = input('За 3 попытки нужно отгадать название музыкальной группы («Queen»).')
-             if roz == 'Queen':
-                 print('Вы выиграли билет на концерт!')
-                 break
+import random
+my_list = []
+for i in range(1000):
+    my_list.append(random.randint(-1000, 1000))
+
+my_list.sort()
+print(my_list[0])
